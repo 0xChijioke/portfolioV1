6814ff1f10318e8994cdfe7d362c432b365833f7
chijoke ogbuanya <chijokeogbuanya@gmail.com>
2022-10-02 20:31:41 +0100

    Hero works

--- components/BackgroundCircles.tsx
+++ components/BackgroundCircles.tsx
@@ -5,12 +5,11 @@ type Props = {}
 
 function BackgroundCircles({}: Props) {
   return (
-    <div className='relative'>
-        <div className="border border-orange-400 rounded-full h-[200px] w-[200px] animate-ping"></div>
-        <div></div>
-        <div />
-        <div />
-        <div />
+    <div className='relative flex justify-center items-center'>
+        <div className="absolute border border-orange-400 rounded-full h-[200px] w-[200px] animate-ping"></div>
+        <div className="absolute border border-orange-400 rounded-full h-[300px] w-[300px]"></div>
+        <div className="absolute border border-orange-400 rounded-full h-[500px] w-[500px]"></div>
+        <div className="absolute border border-orange-400 rounded-full h-[650px] w-[t650px] opacity-20 animate-pulse"></div>
     </div>
   )
 }
