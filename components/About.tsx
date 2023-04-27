09f3a629b9c10685ef773184f6a081e3fae38f9b
EngrGord <chijokeogbuanya@gmail.com>
2023-04-27 21:22:09 +0100

    Added image for About page.

--- components/About.tsx
+++ components/About.tsx
@@ -1,6 +1,8 @@
 import { motion } from 'framer-motion';
 import React from 'react';
 import placeHolderImage from "../assets/placeholder.jpg";
+import Me from "../assets/me.jpg";
+import Image from 'next/image';
 
 type Props = {}
 
@@ -12,8 +14,7 @@ function About({}: Props) {
         transition={{ duration: 1.5 }}
          className='flex flex-col relative h-screen text-center md:text-left md:flex-row max=w=6xl px-10 justify-evenly mx=auto items-center'>
         <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>About</h3>
-        <motion.img 
-            src="https://via.placeholder.com/300"
+        <motion.div
             initial={{
                 x: -200,
                 opacity: 0,
@@ -23,7 +24,13 @@ function About({}: Props) {
             }}
             whileInView={{ opacity: 1, x: 0 }}
             viewport={{ once: true }}
-            className="mt-20 md:mb-0 flex-shrink-0 w-36 h-36 rounded-full object-cover md:rounded-lg md:h-95 md:w-64 lg:w-[300px] lg:h-[400px]" />
+            className="mt-20 md:mb-0 flex-shrink-0 w-36 h-36 rounded-full object-cover md:rounded-lg md:h-95 md:w-64 lg:w-[300px] lg:h-[400px]" >
+          <Image
+            src={Me}
+            width={36}
+            height={36}
+          />
+        </motion.div>
 
             <div className='space-y-10 px-0 md:px-10'>
                 <h4 className='text-2xl font-semibold'>Here is a{" "}
